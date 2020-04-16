9ffa6d39bd35107920b4ed12bbf1346efd613526
Cangyu Lin <jefflin.je598@gmail.com>
2020-04-16 13:56:53 +0800

    add course lab
    error invalid type found

---
+++ TestInst.java
@@ -0,0 +1,12 @@
+public class TestInst{
+	public static void main(String args[]){
+		int a;
+		double b;
+		float c;
+		String d;
+		if(a instanceof Object)
+			System.out.println("a is an instance of Object");
+		if(b instanceof Object)
+			System.out.println("b is an instance of Object");
+	}
+}
