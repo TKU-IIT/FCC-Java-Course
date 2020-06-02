e2270040103c37b4f99e8027ab0802d7d69e4779
Onion0000 <63700616+Onion0000@users.noreply.github.com>
2020-06-02 11:34:57 +0800

    Add files via upload

---
+++ args.java
@@ -0,0 +1,12 @@
+public class args{
+	public static void main(String[] args){
+		
+		//String[] word={args[0], args[1], args[2]};
+		//int x=word.length;
+		
+		System.out.println("args[0]= " +args[0]);
+		System.out.println("args[1]= " +args[1]);
+		System.out.println("args[2]= " +args[2]);
+		System.out.println("string.length= " +args.length);
+	}
+}
